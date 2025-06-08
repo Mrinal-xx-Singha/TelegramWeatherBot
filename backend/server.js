353d6312febf5a10f3a34cb4a61373745bcc395b
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-06-08 12:50:40 +0530

    completed the telegram bot

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,43 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const authRoutes = require("./routes/authRoutes")
-const userRoutes = require("./routes/userRoutes")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-dotenv.config()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}))
+const path = require("path");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // DB Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch((err)=>console.log(err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
+const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
